ed6aecbf4ce0a915719727e7df31e9e4774c0fe7
Z-Zetao <514656083@qq.com>
2022-03-26 13:45:17 +0800

    test

--- src/com/lu/LuZheng.java
+++ src/com/lu/LuZheng.java
@@ -4,5 +4,11 @@ public class LuZheng {
     public static void main(String[] args) {
         System.out.println("LuZheng");
         System.out.println("zzt");
+        int c = add(3,4);
+        System.out.println(c);
+    }
+
+    public static int add(int a, int b){
+        return a + b;
     }
 }
