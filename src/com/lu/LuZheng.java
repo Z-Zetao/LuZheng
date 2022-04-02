d611f27b479fd50ef39cc7a3653f5d7107fc4fc0
Z-Zetao <514656083@qq.com>
2022-04-02 18:06:41 +0800

    测试Math

--- src/com/lu/LuZheng.java
+++ src/com/lu/LuZheng.java
@@ -6,6 +6,7 @@ public class LuZheng {
         System.out.println("zzt");
         int c = add(3,4);
         System.out.println(c);
+        System.out.println(Math.exp(1));
     }
 
     public static int add(int a, int b){
