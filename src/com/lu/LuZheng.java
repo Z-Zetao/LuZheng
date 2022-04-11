fd7feb7e97999021e27f0250a8462488e9f12575
Z-Zetao <514656083@qq.com>
2022-04-11 15:25:36 +0800

    时间测试

--- src/com/lu/LuZheng.java
+++ src/com/lu/LuZheng.java
@@ -1,12 +1,22 @@
 package com.lu;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class LuZheng {
     public static void main(String[] args) {
-        System.out.println("LuZheng");
-        System.out.println("zzt");
-        int c = add(3,4);
-        System.out.println(c);
-        System.out.println(Math.exp(1));
+//        System.out.println("LuZheng");
+//        System.out.println("zzt");
+//        int c = add(3,4);
+//        System.out.println(c);
+//        System.out.println(Math.exp(1));
+
+        System.out.println(new Date());
+
+        Date dNow = new Date();
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(dNow));
+
+
     }
 
     public static int add(int a, int b){
