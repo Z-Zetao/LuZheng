5b7ca3f5ac9246dbb48df0fbb81615e776678a64
Z-Zetao <514656083@qq.com>
2022-05-17 10:31:52 +0800

    2022-05-16

--- src/com/WrapperTest/BaseToWrapper.java
+++ src/com/WrapperTest/BaseToWrapper.java
@@ -5,7 +5,16 @@ package com.WrapperTest;
  */
 public class BaseToWrapper {
     public static void main(String[] args) {
-        Integer integer1 = 12;
+        Integer i = new Integer(1);
+        Integer j = new Integer(1);
+        System.out.println(i == j);//false
 
+        Integer m = 1;
+        Integer n = 1;
+        System.out.println(m == n);//true
+
+        Integer x = 128;
+        Integer y = 128;
+        System.out.println(x == y);//false
     }
 }
